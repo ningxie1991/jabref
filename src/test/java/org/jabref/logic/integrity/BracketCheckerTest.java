7535065ed55a556aa903936723af09c1d8b32d0c
BShaq <bulinshaqiri@hotmail.com>
2021-05-17 10:24:55 +0200

    Convert to parameterized tests

--- src/test/java/org/jabref/logic/integrity/BracketCheckerTest.java
+++ src/test/java/org/jabref/logic/integrity/BracketCheckerTest.java
@@ -1,8 +1,14 @@
 package org.jabref.logic.integrity;
 
 import java.util.Optional;
+import java.util.stream.Stream;
+
+import org.jabref.logic.l10n.Localization;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
@@ -11,53 +17,26 @@ public class BracketCheckerTest {
 
     private final BracketChecker checker = new BracketChecker();
 
-    @Test
-    void fieldAcceptsNoBrackets() {
-        assertEquals(Optional.empty(), checker.checkValue("x"));
+    @ParameterizedTest
+    @MethodSource("provideInput")
+    public void checkBrackets(Optional optValue, String providedInput) {
+        assertEquals(optValue, checker.checkValue(providedInput));
     }
 
-    @Test
-    void fieldAcceptsEvenNumberOfBrackets() {
-        assertEquals(Optional.empty(), checker.checkValue("{x}"));
-    }
-
-    @Test
-    void fieldAcceptsExpectedBracket() {
-        assertEquals(Optional.empty(), checker.checkValue("{x}x{}x{{}}"));
-    }
-
-    @Test
-    void fieldDoesNotAcceptOddNumberOfBrackets() {
-        assertNotEquals(Optional.empty(), checker.checkValue("{x}x{}}x{{}}"));
-    }
-
-    @Test
-    void fieldDoesNotAcceptUnexpectedClosingBracket() {
-        assertNotEquals(Optional.empty(), checker.checkValue("}"));
-    }
-
-    @Test
-    void fieldDoesNotAcceptUnexpectedOpeningBracket() {
-        assertNotEquals(Optional.empty(), checker.checkValue("{"));
-    }
-
-    @Test
-    void fieldAcceptsFirstCharacterNotABracket() {
-        assertEquals(Optional.empty(), checker.checkValue("test{x}"));
-    }
-
-    @Test
-    void fieldAcceptsLastCharacterNotABracket() {
-        assertEquals(Optional.empty(), checker.checkValue("{x}test"));
-    }
-
-    @Test
-    void fieldAcceptsFirstAndLastCharacterNotABracket() {
-        assertEquals(Optional.empty(), checker.checkValue("test{x}test"));
-    }
-
-    @Test
-    void fieldAcceptsEmptyInput() {
-        assertEquals(Optional.empty(), checker.checkValue(""));
+    private static Stream<Arguments> provideInput() {
+        return Stream.of(
+                // valid (bracketed) String
+                Arguments.of(Optional.empty(), "x"),
+                Arguments.of(Optional.empty(), "{x}"),
+                Arguments.of(Optional.empty(), "{x}x{}x{{}}"),
+                Arguments.of(Optional.empty(), "test{x}"),
+                Arguments.of(Optional.empty(), "{x}test"),
+                Arguments.of(Optional.empty(), "test{x}test"),
+                Arguments.of(Optional.empty(), ""),
+                // invalid string
+                Arguments.of(Optional.of(Localization.lang("unexpected closing curly bracket")), "{x}x{}}x{{}}"),
+                Arguments.of(Optional.of(Localization.lang("unexpected closing curly bracket")), "}"),
+                Arguments.of(Optional.of(Localization.lang("unexpected opening curly bracket")), "{")
+        );
     }
 }
