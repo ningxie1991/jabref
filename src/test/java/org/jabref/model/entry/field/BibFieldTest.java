94a6b039a6e897fd5feb9a936c922d1b39a5fa34
ningxie <ning.xie@uzh.ch>
2021-06-01 15:30:56 +0200

    Refactored BibFieldTest
    Test code smell: sensitive equality

--- src/test/java/org/jabref/model/entry/field/BibFieldTest.java
+++ src/test/java/org/jabref/model/entry/field/BibFieldTest.java
@@ -3,17 +3,23 @@ package org.jabref.model.entry.field;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class BibFieldTest {
 
     @Test
     void bibFieldsConsideredEqualIfUnderlyingFieldIsEqual() {
-        assertEquals(new BibField(StandardField.AUTHOR, FieldPriority.IMPORTANT), new BibField(StandardField.AUTHOR, FieldPriority.DETAIL));
+        BibField fieldOne = new BibField(StandardField.AUTHOR, FieldPriority.IMPORTANT);
+        BibField fieldTwo = new BibField(StandardField.AUTHOR, FieldPriority.DETAIL);
+        assertTrue(fieldOne.equals(fieldTwo));
     }
 
     @Test
     void bibFieldsConsideredNotEqualIfUnderlyingFieldNotEqual() {
-        assertNotEquals(new BibField(StandardField.AUTHOR, FieldPriority.IMPORTANT), new BibField(StandardField.TITLE, FieldPriority.IMPORTANT));
+        BibField fieldOne = new BibField(StandardField.AUTHOR, FieldPriority.IMPORTANT);
+        BibField fieldTwo = new BibField(StandardField.TITLE, FieldPriority.IMPORTANT);
+        assertFalse(fieldOne.equals(fieldTwo));
     }
 }
