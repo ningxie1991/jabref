21158f70ebccff49551eb9c056651db466f87e57
ningxie <ning.xie@uzh.ch>
2021-06-01 14:54:16 +0200

    Refactored HTMLCharacterCheckerTest

--- src/test/java/org/jabref/logic/integrity/HTMLCharacterCheckerTest.java
+++ src/test/java/org/jabref/logic/integrity/HTMLCharacterCheckerTest.java
@@ -2,11 +2,14 @@ package org.jabref.logic.integrity;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Stream;
 
 import org.jabref.model.entry.BibEntry;
 import org.jabref.model.entry.field.StandardField;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -15,40 +18,22 @@ public class HTMLCharacterCheckerTest {
     private final HTMLCharacterChecker checker = new HTMLCharacterChecker();
     private final BibEntry entry = new BibEntry();
 
-    @Test
-    void titleAcceptsNonHTMLEncodedCharacters() {
-        entry.setField(StandardField.TITLE, "Not a single {HTML} character");
-        assertEquals(Collections.emptyList(), checker.check(entry));
+    private static Stream<Arguments> getParameters() {
+        return Stream.of(
+                Arguments.of(Collections.emptyList(), new BibEntry().withField(StandardField.TITLE, "Not a single {HTML} character")),
+                Arguments.of(Collections.emptyList(), new BibEntry().withField(StandardField.MONTH, "#jan#")),
+                Arguments.of(Collections.emptyList(), new BibEntry().withField(StandardField.AUTHOR, "A. Einstein and I. Newton")),
+                Arguments.of(Collections.emptyList(), new BibEntry().withField(StandardField.URL, "http://www.thinkmind.org/index.php?view=article&amp;articleid=cloud_computing_2013_1_20_20130")),
+                Arguments.of(List.of(new IntegrityMessage("HTML encoded character found", new BibEntry().withField(StandardField.AUTHOR, "Lenhard, J&#227;rg"), StandardField.AUTHOR)),
+                        new BibEntry().withField(StandardField.AUTHOR, "Lenhard, J&#227;rg")),
+                Arguments.of(List.of(new IntegrityMessage("HTML encoded character found", new BibEntry().withField(StandardField.JOURNAL, "&Auml;rling Str&ouml;m for &#8211; &#x2031;"), StandardField.JOURNAL)),
+                        new BibEntry().withField(StandardField.JOURNAL, "&Auml;rling Str&ouml;m for &#8211; &#x2031;"))
+        );
     }
 
-    @Test
-    void monthAcceptsNonHTMLEncodedCharacters() {
-        entry.setField(StandardField.MONTH, "#jan#");
-        assertEquals(Collections.emptyList(), checker.check(entry));
+    @ParameterizedTest
+    @MethodSource("getParameters")
+    void test(List expectedResult, BibEntry entry) {
+        assertEquals(expectedResult, checker.check(entry));
     }
-
-    @Test
-    void authorAcceptsNonHTMLEncodedCharacters() {
-        entry.setField(StandardField.AUTHOR, "A. Einstein and I. Newton");
-        assertEquals(Collections.emptyList(), checker.check(entry));
-    }
-
-    @Test
-    void urlAcceptsNonHTMLEncodedCharacters() {
-        entry.setField(StandardField.URL, "http://www.thinkmind.org/index.php?view=article&amp;articleid=cloud_computing_2013_1_20_20130");
-        assertEquals(Collections.emptyList(), checker.check(entry));
-    }
-
-    @Test
-    void authorDoesNotAcceptHTMLEncodedCharacters() {
-        entry.setField(StandardField.AUTHOR, "Lenhard, J&#227;rg");
-        assertEquals(List.of(new IntegrityMessage("HTML encoded character found", entry, StandardField.AUTHOR)), checker.check(entry));
-    }
-
-    @Test
-    void journalDoesNotAcceptHTMLEncodedCharacters() {
-        entry.setField(StandardField.JOURNAL, "&Auml;rling Str&ouml;m for &#8211; &#x2031;");
-        assertEquals(List.of(new IntegrityMessage("HTML encoded character found", entry, StandardField.JOURNAL)), checker.check(entry));
-    }
-
 }
